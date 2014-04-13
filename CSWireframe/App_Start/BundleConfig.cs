5340b2d52508607a9e6b7ee46a2d5b882e1008a2
David Cabiness <dcabines@gmail.com>
2014-04-13 19:16:50 -0400

    Libraries added

---
+++ CSWireframe/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace CSWireframe {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/Scripts/base").Include(
+                        "~/Scripts/jquery-2.1.0",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/knockout-3.1.0"));
+
+            bundles.Add(new StyleBundle("~/bundles/Styles/base").Include(
+                        "~/Styles/bootstrap.css"));
+        }
+    }
+}
